5eaf3e9ca7d2b9eec2b407182b9f0fc43c088eb4
Amistad Alex (ERNI) <amal@erni.ph>
2018-04-03 18:55:32 +0800

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,28 @@ import { NgModule } from '@angular/core';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
 
 import { environment } from '../environments/environment';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { HomeComponent } from './page/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'user/signup', component: SignUpComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
